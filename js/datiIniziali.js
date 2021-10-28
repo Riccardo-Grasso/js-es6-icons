6fe188c2f1f50984d459a0819310f29b040d664f
Riccardo-Grasso <riccardo.grasso@outlook.it>
2021-10-28 22:25:40 +0200

    colori

--- js/datiIniziali.js
+++ js/datiIniziali.js
@@ -99,18 +99,25 @@ const iconsList = [
 
 
 const iconContainer = document.querySelector(".icon-container");
+const colors = {
+	"animal": "blue",
+	"vegetable": "orange",
+	"user": "purple",
+};
 
 function stampaIcone(list) {
 
 	for (let i = 0; i < list.length; i++) {
 
 		const icon = list[i];
+		const { name, prefix, family, type } = icon;
+		const color = colors[type];
 
 		iconContainer.innerHTML += `
-    <div class="col d-flex justify-content-center">
-      <div class="card w-75 my-rounded">
+    <div class="col d-flex justify-content-center my-4">
+      <div class="card my-width my-rounded">
         <div class="card-img-top d-flex justify-content-center align-items-center pt-4">
-          <i class="${icon.family} ${icon.prefix + icon.name} fs-1"></i>
+          <i class="${family} ${prefix + name} fs-1" style="color: ${color}" ></i>
         </div>
         <div class="card-body py-2">
           <div class="card-title text-center fw-bold">${icon.name.toUpperCase()}</div>
