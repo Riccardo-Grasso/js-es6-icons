3cf9863ddfdf719a7113bd36870dd1f6b6943f32
Riccardo-Grasso <riccardo.grasso@outlook.it>
2021-10-28 17:12:14 +0200

    stampa

--- js/datiIniziali.js
+++ js/datiIniziali.js
@@ -1,4 +1,4 @@
-[
+const iconsList = [
 	{
 		name: 'cat',
 		prefix: 'fa-',
@@ -99,3 +99,26 @@
 
 
 const iconContainer = document.querySelector(".icon-container");
+
+function stampaIcone(list) {
+
+	for (let i = 0; i < list.length; i++) {
+
+		const icon = list[i];
+
+		iconContainer.innerHTML += `
+    <div class="col d-flex justify-content-center">
+      <div class="card w-75 my-rounded">
+        <div class="card-img-top d-flex justify-content-center align-items-center pt-4">
+          <i class="${icon.family} ${icon.prefix + icon.name} fs-1"></i>
+        </div>
+        <div class="card-body py-2">
+          <div class="card-title text-center fw-bold">${icon.name.toUpperCase()}</div>
+        </div>
+      </div>
+    </div>`;
+
+	}
+}
+
+stampaIcone(iconsList);
