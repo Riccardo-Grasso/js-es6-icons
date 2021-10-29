63b189d5f7465fc0b6259d7dda1490c2ba87c8c5
Riccardo-Grasso <riccardo.grasso@outlook.it>
2021-10-29 17:31:12 +0200

    finito

--- js/datiIniziali.js
+++ js/datiIniziali.js
@@ -99,12 +99,18 @@ const iconsList = [
 
 
 const iconContainer = document.querySelector(".icon-container");
-const filterSelect = document.querySelectorAll(".filter_select");
+const filterSelect = document.getElementById("filter_select");
 
 console.log(filterSelect);
 
-filterSelect.addEventListener("click", function () {
+filterSelect.addEventListener("change", renderPage);
 
+
+
+function renderPage() {
+
+	console.log(this.value);
+	let filtro = this.value || "all";
 
 	const colors = {
 		"animal": "blue",
@@ -131,41 +137,43 @@ filterSelect.addEventListener("click", function () {
 				}
 			});
 		}
-		console.log(listaGruppi);
-		return listaGruppi;
-
+		stampaSingolaCategoria(listaGruppi);
 	}
 
 	function stampaSingolaCategoria(listaDaStampare) {
 		iconContainer.innerHTML = "";
-		for (let i = 0; i < listaDaStampare.length; i++) {
 
-			const icon = listaDaStampare[i];
-			const { name, prefix, family, color } = icon;
+		for (let prop in listaDaStampare) {
 
-			iconContainer.innerHTML += `
-		<div class="col d-flex justify-content-center my-4">
-			<div class="card my-width my-rounded py-5">
-				<div class="card-img-top d-flex justify-content-center align-items-center pt-4">
-					<i class="${family} ${prefix + name} fs-1" style="color: ${color()}" ></i>
-				</div>
-				<div class="card-body py-2">
-					<div class="card-title text-center fw-bold">${name.toUpperCase()}</div>
-				</div>
-			</div>
-		</div>`;
+			if (filtro === "all" || filtro === prop) {
 
+
+				for (let i = 0; i < listaDaStampare[prop].length; i++) {
+
+					const icon = listaDaStampare[prop][i];
+					const { name, prefix, family, color } = icon;
+
+					iconContainer.innerHTML += `
+					<div class="col d-flex justify-content-center my-4">
+						<div class="card my-width my-rounded py-5">
+							<div class="card-img-top d-flex justify-content-center align-items-center pt-4">
+								<i class="${family} ${prefix + name} fs-1" style="color: ${color()}" ></i>
+							</div>
+							<div class="card-body py-2">
+								<div class="card-title text-center fw-bold">${name.toUpperCase()}</div>
+							</div>
+						</div>
+					</div>`;
+
+				}
+			}
 		}
 	}
-
-	const filtro = filterSelect.value;
-	console.log(filtro);
-
 	const iconeRaggruppate = raggruppaPerCategoria(iconsList);
 
 	console.log(iconeRaggruppate);
-	stampaSingolaCategoria(iconeRaggruppate.user);
-});
+}
+renderPage();
 
 /* const iconContainer = document.querySelector(".icon-container");
 const colors = {
