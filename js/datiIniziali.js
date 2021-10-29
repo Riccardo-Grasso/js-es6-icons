131f2734c4006568d4be14a5de2f3e87d30d6870
Riccardo-Grasso <riccardo.grasso@outlook.it>
2021-10-29 12:51:14 +0200

    update

--- js/datiIniziali.js
+++ js/datiIniziali.js
@@ -99,6 +99,75 @@ const iconsList = [
 
 
 const iconContainer = document.querySelector(".icon-container");
+const filterSelect = document.querySelectorAll(".filter_select");
+
+console.log(filterSelect);
+
+filterSelect.addEventListener("click", function () {
+
+
+	const colors = {
+		"animal": "blue",
+		"vegetable": "orange",
+		"user": "purple",
+	};
+
+	function raggruppaPerCategoria(iconsList) {
+		const listaGruppi = {};
+		for (let i = 0; i < iconsList.length; i++) {
+			const { type, name, prefix, family } = iconsList[i];
+
+
+			if (!listaGruppi[type]) {
+				listaGruppi[type] = [];
+			}
+
+			listaGruppi[type].push({
+				"name": name,
+				"prefix": prefix,
+				family,
+				color() {
+					return colors[type];
+				}
+			});
+		}
+		console.log(listaGruppi);
+		return listaGruppi;
+
+	}
+
+	function stampaSingolaCategoria(listaDaStampare) {
+		iconContainer.innerHTML = "";
+		for (let i = 0; i < listaDaStampare.length; i++) {
+
+			const icon = listaDaStampare[i];
+			const { name, prefix, family, color } = icon;
+
+			iconContainer.innerHTML += `
+		<div class="col d-flex justify-content-center my-4">
+			<div class="card my-width my-rounded py-5">
+				<div class="card-img-top d-flex justify-content-center align-items-center pt-4">
+					<i class="${family} ${prefix + name} fs-1" style="color: ${color()}" ></i>
+				</div>
+				<div class="card-body py-2">
+					<div class="card-title text-center fw-bold">${name.toUpperCase()}</div>
+				</div>
+			</div>
+		</div>`;
+
+		}
+	}
+
+	const filtro = filterSelect.value;
+	console.log(filtro);
+
+	const iconeRaggruppate = raggruppaPerCategoria(iconsList);
+
+	console.log(iconeRaggruppate);
+	stampaSingolaCategoria(iconeRaggruppate.user);
+});
+
+/* const iconContainer = document.querySelector(".icon-container");
 const colors = {
 	"animal": "blue",
 	"vegetable": "orange",
@@ -114,18 +183,18 @@ function stampaIcone(list) {
 		const color = colors[type];
 
 		iconContainer.innerHTML += `
-    <div class="col d-flex justify-content-center my-4">
-      <div class="card my-width my-rounded py-5">
-        <div class="card-img-top d-flex justify-content-center align-items-center pt-4">
-          <i class="${family} ${prefix + name} fs-1" style="color: ${color}" ></i>
-        </div>
-        <div class="card-body py-2">
-          <div class="card-title text-center fw-bold">${icon.name.toUpperCase()}</div>
-        </div>
-      </div>
-    </div>`;
+	<div class="col d-flex justify-content-center my-4">
+	  <div class="card my-width my-rounded py-5">
+		<div class="card-img-top d-flex justify-content-center align-items-center pt-4">
+		  <i class="${family} ${prefix + name} fs-1" style="color: ${color}" ></i>
+		</div>
+		<div class="card-body py-2">
+		  <div class="card-title text-center fw-bold">${icon.name.toUpperCase()}</div>
+		</div>
+	  </div>
+	</div>`;
 
 	}
 }
 
-stampaIcone(iconsList);
+stampaIcone(iconsList); */
